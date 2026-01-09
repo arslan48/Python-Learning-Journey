698e6d6dfad3c2d21ee07609228bb42c443b9473
Muhammad Arslan <ma7448487@gmail.com>
2026-01-09 12:20:22 +0500

    day -13

---
+++ String.py
@@ -0,0 +1,6 @@
+my_string = "Python_Expert"
+print(len(my_string))
+print(my_string[-13])
+print(my_string[0:6])
+print(my_string[7:])
+print(my_string[::-1])
---
+++ string1.py
@@ -0,0 +1,5 @@
+secret_message = "7700_is_Password"
+print(len(secret_message))
+print(secret_message[0:4])
+print(secret_message[15])
+print(secret_message[3::-1])
