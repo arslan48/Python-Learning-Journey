8367c02b112fcb3f3af82781285196d6bd963417
Muhammad Arslan <ma7448487@gmail.com>
2026-01-10 09:29:07 +0500

    Delete split.py

--- split.py
+++
@@ -1,7 +0,0 @@
-order_info = "Customer:Zain_Item:Laptop_Price:50000_Status:Paid"
-parts = order_info.split("_")
-sparts = order_info.split(":")
-status = sparts[-1]
-reverse_price = sparts[-2][::-1]
-print(status)
-print(reverse_price)
