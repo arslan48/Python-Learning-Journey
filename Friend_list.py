4659dedef61802d38074eda2280ea24190055354
Muhammad Arslan <ma7448487@gmail.com>
2026-01-13 11:11:57 +0500

    Add script to manage and display friends list
    Introduces Friend_list.py, which demonstrates list operations such as accessing elements, updating values, appending new items, and displaying the list and its length.

---
+++ Friend_list.py
@@ -0,0 +1,11 @@
+Friends =["Ali", "Ahmed", "Ammar", "Hassan", "Hussain"]
+Access_first = Friends[0]
+print("First Friend:", Access_first)
+Access_Third = Friends[2]
+print("Third Friend:", Access_Third)
+Access_Last = Friends[-1]
+print("Last Friend:", Access_Last)
+Friends[1] = "Zain"
+Friends.append("Osama")
+print("Length of Friends List:", len(Friends))
+print("Updated Friends List:", Friends)
