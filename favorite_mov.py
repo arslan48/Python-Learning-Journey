54aafec5e070878488a554d790c5a16388ac34ee
Muhammad Arslan <ma7448487@gmail.com>
2026-01-14 11:46:53 +0500

    Add script to collect favorite movies
    Created favorite_mov.py to prompt user for three favorite movies and display them in a list.

---
+++ favorite_mov.py
@@ -0,0 +1,8 @@
+Movies = []
+Movie1=input("Enter your first favorite movie: ")
+Movie2 =input("Enter your second favorite movie: ")
+Movie3 =input("Enter your third favorite movie: ")
+Movies.append(Movie1)
+Movies.append(Movie2)
+Movies.append(Movie3)
+print(Movies)
