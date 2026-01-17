40ff819e54f4a6465c9cb29eda1ca2f7784a1f17
arslan48 <ma7448487@gmail.com>
2026-01-17 10:26:29 +0500

    Add script to merge and display user profile tuple
    Introduces tuple_profile_merge.py, which unpacks user data, merges it with skills, checks for a skill's presence, and prints the results.

---
+++ tuple_profile_merge.py
@@ -0,0 +1,8 @@
+user_data = ("Zain", 25, "Programmer", "Islamabad")
+name, age, profession, city = user_data
+print(f"{name} is {age} years old {profession} and lives in {city}")
+skills =("python","java")
+final_profile = user_data + skills
+The_serch = "python" in final_profile
+print(The_serch)
+print(final_profile)
