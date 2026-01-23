0a82f2d4d4380d16867b32d8cf9b33a4ac21b782
arslan48 <ma7448487@gmail.com>
2026-01-23 11:55:00 +0500

    Add set operations example in set2.py
    Introduces a new Python script demonstrating basic set operations such as add, pop, and remove, as well as manipulation of a set of names.

---
+++ Dictionary & Set in Python/set2.py
@@ -0,0 +1,18 @@
+new_set = set()
+Names = { "Zain", "Ali", "Asim", "zahra", "umer"}
+Names.add("Ahmed")
+print(Names)
+
+new_set.add(1)
+
+new_set.add(2)
+new_set.add(5)
+new_set.add(3)
+new_set.add(4)
+new_set.add(5)
+new_set.add(4)
+new_set.pop()
+
+new_set.remove(2)
+print(new_set)
+
