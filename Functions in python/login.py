aadd09d618a076565e73abb7c810798a0f210934
arslan48 <ma7448487@gmail.com>
2026-03-05 10:17:47 +0500

    fix the logic

--- Functions in python/login.py
+++ Functions in python/login.py
@@ -1,8 +1,10 @@
-def login():
-    Username = "Ahmed"
-    password = "1234"
+def login_system():
     for i in range(3):
-        username_input = input("Enter the name")
-        if 
-   
-    
+        username = input("Enter name: ")
+        password = input("Enter the password:")
+        if  username == "Ahmed" and password == "ffhr12":
+            return "Login sucessfull"
+        else:
+            print(f"please try again {2 -i} attempts are left ")
+    return "Account blocked"
+print(login_system())
