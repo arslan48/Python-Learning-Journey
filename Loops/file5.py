efc3eb39df40e968f07191f291c05d22e2655510
arslan48 <ma7448487@gmail.com>
2026-03-22 09:48:21 +0500

    basic while exameple with break statement

---
+++ Loops/file5.py
@@ -0,0 +1,9 @@
+while True:
+    number = int(input("Enter the number"))
+    name = input("Enter the name")
+    if name == "Asim" and number == 55:
+        print("WWow")
+        break
+    else:
+        print("what the helll are you doing")
+
