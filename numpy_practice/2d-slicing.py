e1df704eab005adf686d1aacc8820a3840d18fbf
arslan48 <ma7448487@gmail.com>
2026-03-25 12:35:11 +0500

    Add basic example of slicing

---
+++ numpy_practice/2d-slicing.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+arr2d = np.array([
+    [1,  2,  3,  4],
+    [5,  6,  7,  8],
+    [9, 10, 11, 12]
+])
+
+print(arr2d[0:3,-1])
---
+++ numpy_practice/slicing.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+arr = np.array([10, 20, 30, 40, 50,60,70,80,90,100])
+
+print(arr[1:3])
+
+print(arr[-1])
+
+print(arr[-3:])
+
+print(arr[-7:-3])
+
+print(arr[8])
