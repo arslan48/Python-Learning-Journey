e0bab13abe3c90889ab3188d682713a6e3b827e3
arslan48 <ma7448487@gmail.com>
2026-03-25 12:06:01 +0500

    Add basic example of numpy indexing

---
+++ numpy_practice/Numpy_indexing.py
@@ -0,0 +1,14 @@
+import numpy as np
+arr = np.array([10,20,30,40,50])
+print(arr[0])
+print(arr[-1])
+print(arr[2])
+
+arr2d = np.array([
+    [1,2,3],
+    [4,5,6]
+])
+
+print(arr2d[0][1])
+
+print(arr2d[1][2])
