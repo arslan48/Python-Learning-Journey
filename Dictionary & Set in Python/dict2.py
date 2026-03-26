a89d5439080e5624a380a9483921db7635e98133
arslan48 <ma7448487@gmail.com>
2026-03-26 11:44:46 +0500

    Add dict basic

---
+++ Dictionary & Set in Python/dict2.py
@@ -0,0 +1,24 @@
+fruit = {
+    "name1": "Apple",
+    "name2": "Grapes",
+    "name3": "Watermelon"
+}
+fruit["name3"] = "Melon"
+fruit.update
+print(fruit.values())
+
+print(fruit)
+print(fruit.keys())
+
+rem = fruit.pop("name2")
+
+print(rem)
+
+print(fruit)
+
+
+print(fruit)
+# fruit.clear()
+
+# print(fruit)
+
