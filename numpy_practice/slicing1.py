89ab6546c822cd3b2e5896d63cede0905567f525
arslan48 <ma7448487@gmail.com>
2026-03-31 12:15:05 +0500

    slicing in numpy

---
+++ numpy_practice/slicing1.py
@@ -0,0 +1,24 @@
+import numpy as np
+
+scores = np.array([
+    [85, 90, 88], # Index 0
+    [70, 75, 72], # Index 1
+    [95, 92, 98], # Index 2
+    [60, 65, 62]  # Index 3
+])
+
+sara_marks = scores[1,:]
+
+print(sara_marks)
+
+jan_marks = scores[:,0]
+
+print(jan_marks)
+
+zain_feb = scores[2,1]
+
+print(zain_feb)
+
+hina_march = scores[3,2]
+
+print(hina_march)
