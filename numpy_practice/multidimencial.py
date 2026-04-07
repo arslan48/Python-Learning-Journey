43af048898a2d1eadc460a6c0d20bcf80808418f
arslan48 <ma7448487@gmail.com>
2026-04-07 11:04:23 +0500

    NumPy indexing - 1D 2D 3D practice

--- numpy_practice/multidimencial.py
+++ numpy_practice/multidimencial.py
@@ -27,3 +27,27 @@ print(sit)
 name = array[0,0,0] + array[1,0,2] + array[0,2,2] + array[0,0,2] + array[0,1,1]
 
 print(name)
+# 1D
+fruits = np.array(['Apple', 'Mango', 'Banana', 'Orange', 'Grapes'])
+
+# 2D
+students = np.array([['John',   '85', 'A'],
+                     ['Emma',   '92', 'A'],
+                     ['Peter',  '45', 'F'],
+                     ['Sophie', '78', 'B']])
+
+fru = fruits[2] + " & " + fruits[-1]
+
+print(f"Fruit {fru}")
+
+stud1 = students[1,1]
+
+print(stud1)
+
+stud2 = students[2,2]
+
+print(stud2)
+
+names =students[:,0]
+
+print(names)
