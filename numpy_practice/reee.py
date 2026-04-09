e5db51f573c1246927e60205d493d5d6f9ada370
arslan48 <ma7448487@gmail.com>
2026-04-09 11:03:47 +0500

    Update reee.py

--- numpy_practice/reee.py
+++ numpy_practice/reee.py
@@ -19,3 +19,13 @@ arr = np.array([1,2,3,4,5,6,7,8,9,10,11,12])
 print(f"arr reshape\n{arr.reshape(3,4)}")
 
 print(f"3d arr reshape\n{arr.reshape(2,2,3)}")
+
+print(f"inferred shape\n{arr.reshape(6,-1)}")
+print(f"inferred shape\n{arr.reshape(-1,3)}")
+
+arr2 = np.array([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20])
+print(f"arr2\n{arr2.reshape(2,-1)}")
+
+arr3 = np.array([10, 20, 30, 40, 50, 60])
+reshp = arr3.reshape((3,2), order ='F')
+print(reshp)
