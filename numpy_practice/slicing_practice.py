8cea16c9a4e6c19858e54a2685dcb8583520f646
arslan48 <ma7448487@gmail.com>
2026-04-11 10:44:39 +0500

    update

--- numpy_practice/slicing_practice.py
+++ numpy_practice/slicing_practice.py
@@ -8,8 +8,19 @@ print(f"first two rows:\n{arr[0:2]}")
 
 print(f"last row:\n{arr[-1:]}")
 
-print(f"column 1 and 2 full rows:\n{arr[0:,1:3]}")
+print(f"column 1 and 2 full rows:\n{arr[:,1:3]}")
 
 print(f"Row 1 and 2 column 0 and 1:\n{arr[1:3,0:2]}")
 
-print()
+print(f"skip:\n{arr[::2]}")
+
+arr1 = np.array([[10, 20, 30, 40],
+                [50, 60, 70, 80],
+                [90, 100, 110, 120],
+                [130, 140, 150, 160]])
+
+print(f"last two rows:\n{arr1[2:4]}")
+
+print(f"first two rows last two column{arr1[0:2,2:]}")
+
+print(f"skip:\n{arr1[:,::2]}")
