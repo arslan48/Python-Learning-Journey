2a7fbcd7a8cee506bf5ab42ab7b58c2acb2a9c0d
arslan48 <ma7448487@gmail.com>
2026-04-13 14:26:35 +0500

    basic example

--- numpy_practice/arithmetic/scaler.py
+++ numpy_practice/arithmetic/scaler.py
@@ -2,5 +2,12 @@ import numpy as np
 
 prices = np.array([100, 200, 300, 400, 500])
 
-tex = pricet
-s
+tex = prices *1.10
+
+discount = prices[2:] -5
+
+half = prices /2
+
+print(f"Tex added:\n{tex}\n")
+print(f"Discount on last 3:\n{discount}\n")
+print(f"Half price:\n{half}")
