7b318ba40d8135b8ea0bcb634e10013b9730134c
arslan48 <ma7448487@gmail.com>
2026-04-13 15:06:24 +0500

    Add numpy arithmetic practice scripts

---
+++ numpy_practice/arithmetic/find _area.py
@@ -0,0 +1,6 @@
+import numpy as np
+
+radi = np.array([2,-2,0,-4])
+
+print(np.pi * radi **2)
+
---
+++ numpy_practice/arithmetic/vactorize.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+arr = np.array([1,4,9,16,25])
+
+print(np.square(arr))
+
+print(np.abs(arr))
+
+print(np.pi)
