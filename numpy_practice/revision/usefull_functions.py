e97cd7029aecccb58f3e851c611fee2f5af2efaf
arslan48 <ma7448487@gmail.com>
2026-04-30 09:09:47 +0500

    Create usefull_functions.py

---
+++ numpy_practice/revision/usefull_functions.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+array = np.ones((3,10))
+print(array)
+
+array2 = np.empty((5,4))
+print(array2)
+
+array3 = np.linspace(0,10,2)
+print(array3)
+
+array4 = np.arange(0,100,10)
+print(array4)
