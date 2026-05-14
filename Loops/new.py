fd86a0f7fe5a63857f06c51fd0b46e4c51440668
arslan48 <ma7448487@gmail.com>
2026-05-14 14:29:44 +0500

    add: while loop practice

--- Loops/new.py
+++ Loops/new.py
@@ -2,3 +2,20 @@ import numpy as np
 arr = np.array([33,34,34,83,24])
 for i in arr:
     print(np.sqrt(i))
+
+# Numpy + For loop + condition
+
+arr1 = np.array([10,15,10,30,34,50,40])
+for  num in arr1:
+    if num > 25:
+        print(num)
+    
+# Find maximum Number
+
+numbers = np.array([34, 67, 23, 89, 12, 78])
+max_num = 0
+for num in numbers:
+    if num > max_num:
+        max_num = num
+
+print(f"Max number: {max_num}")
---
+++ Loops/while_loop.py
@@ -0,0 +1,13 @@
+# basic
+i = 1
+while i <=10:
+    print(i)
+    i = i + 1
+
+# user input 
+
+number = int(input("Enter the number: "))
+while number != 0:
+    print(number)
+    
+
