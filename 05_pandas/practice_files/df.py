c6256a44d2963dcae060a40fc40c7f6ff0f208d6
arslan48 <ma7448487@gmail.com>
2026-05-20 09:06:21 +0500

    Create df.py

---
+++ 05_pandas/practice_files/df.py
@@ -0,0 +1,14 @@
+import pandas as pd
+
+data = {
+    "Name": ["Alice","Bob","Carlo","Max"],
+    "Age": [43,34,54,65],
+    "Country": ["Japan","England","China","Iran"]
+}
+
+df = pd.DataFrame(data,index=range(1,5))
+# print(df)
+
+print(df.iloc[0])
+
+print(df["Age"])
