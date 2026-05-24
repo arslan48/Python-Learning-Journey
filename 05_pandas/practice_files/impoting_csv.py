e68e01a0eb76279dfeccc9b6704c51122df1a184
arslan48 <ma7448487@gmail.com>
2026-05-24 05:17:28 +0500

    pandas csv and selection practice

--- 05_pandas/practice_files/impoting_csv.py
+++ 05_pandas/practice_files/impoting_csv.py
@@ -1,9 +1,24 @@
 import pandas as pd
 
 df = pd.read_csv("students_150.csv",index_col="name")
-anime = pd.read_csv("anime.csv",index_col="AnimeTitle")
-print(anime)
+anime = pd.read_csv("anime.csv")
+
 
 # Selection 
 
-print(anime.loc["One Piece":"Bleach",["Rating","Status"]])
+print("\n==Dataframe==")
+print(anime)
+
+print("\n==Title and Rating")
+print(anime[["AnimeTitle","Rating"]])
+
+print("\n==Rating above 9.0+==")
+print(anime[anime["Rating"]>=9.0])
+
+print("\n==Anime(s) with Most Episodes==")
+max_eps = anime["Episodes"].max()
+print(f"Most episodes: {max_eps}")
+print(anime[anime["Episodes"] == max_eps])
+
+print("\n==Anime made by MAPPA studio==")
+print(anime[anime["Studio"]=="MAPPA"])
