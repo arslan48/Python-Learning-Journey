2143b23be21f4d8c7cc70c6786e93e55d90e4607
arslan48 <ma7448487@gmail.com>
2026-06-25 20:29:34 +0500

    slicing_in_numpy

--- 04_numpy/practice_files/basis/basic.py
+++ 04_numpy/practice_files/basis/basic.py
@@ -7,10 +7,22 @@ print(f"replace 24 with {change}")
 slicing = a[0:3]
 print(slicing)
 b = np.array([
-            [12,3,2,3],
-            [23,4,5,8]
-            ])
+    [12,3,2,3],
+    [23,4,5,8]
+    ])
 print(b.shape)
 nd = b.ndim
 print(nd)
+
+c = np.array([
+    [2,4,5,3],
+    [4,4,5,7],
+    [12,34,23,65]
+    ])
+#slic = c[:,2:4]
+#print(slic)
+slic = c[1:2,2:4]
+print(slic)
+last_row = c[2:3,3:4]
+print(last_row)
 
