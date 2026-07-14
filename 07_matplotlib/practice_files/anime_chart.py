212bf8dad9e4892300703934fcf1834b34dd870b
Muhammad Arslan <ma7448487@gmail.com>
2026-07-14 08:12:12 +0500

    Update anime_chart.py

--- 07_matplotlib/practice_files/anime_chart.py
+++ 07_matplotlib/practice_files/anime_chart.py
@@ -5,7 +5,7 @@ anime_df = pd.read_csv("anime_full.csv")
 x = anime_df["Title"]
 y = anime_df["Rating"] 
 
-plt.plot(x,y,color="purple",linewidth=2,marker="s")
+plt.plot(x,y,color="green",linewidth=2,marker="s")
 plt.title("Anime Data")
 plt.xlabel("Anime")
 plt.ylabel("Rating")
