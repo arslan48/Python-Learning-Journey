212e7f16177b9445f243f228d5faf049cce86b3a
Muhammad Arslan <ma7448487@gmail.com>
2026-07-17 06:28:33 +0500

    Update matplotib_practice.py

--- 07_matplotlib/practice_files/matplotib_practice.py
+++ 07_matplotlib/practice_files/matplotib_practice.py
@@ -1,5 +1,16 @@
 import matplotlib.pyplot as plt
 x = [2023,2024,2024,2025,2026]
-y = [15,32,34,83,0]
-plt.plot(x,y)
+y1 = [25,30,45,60,0]
+y2 = [10,24,13,39,10]
+y3 = [15,20,25,30,35]
+line_style = dict(marker=".",
+	     markersize=30,
+	     mfc="#34ebb1",
+		 mec="#34ebb1",
+		 linestyle="solid",
+		 linewidth=4,
+)
+plt.plot(x,y1,color="#c334eb",**line_style)
+plt.plot(x,y2,color="#4287f5",**line_style)
+plt.plot(x,y3,color="#9a75f0",**line_style)
 plt.show()
