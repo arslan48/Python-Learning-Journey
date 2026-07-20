f8b69589bd0d0268aa030c5a9e31fbb20387a58d
Muhammad Arslan <ma7448487@gmail.com>
2026-07-20 06:09:31 +0500

    Update ratings.py

--- 07_matplotlib/practice_files/ratings.py
+++ 07_matplotlib/practice_files/ratings.py
@@ -5,7 +5,7 @@ naruto = np.array([70, 65, 60, 58])
 onepiece = np.array([80, 85, 90, 95])
 aot = np.array([90, 88, 75, 70])
 
-line_style = dict(marker=".",markersize=9,linewidth=2)
+line_style = dict(marker=".",markersize=10,linewidth=2)
 
 plt.title("Anime Ratings",fontsize=22,family="arial",fontweight="bold",color="#8839ef")
 plt.xlabel("Years",fontsize=12,family="arial",color="#8839ef")
@@ -16,6 +16,8 @@ plt.tick_params(axis="both",colors="#8839ef")
 plt.plot(years, naruto,color="#fe640b",label="Naruto",**line_style)
 plt.plot(years,onepiece,color="#8839ef",label="One piece",**line_style)
 plt.plot(years,aot,color="#ea76cb",label="AOT",**line_style)
+
+plt.grid(axis="both",linewidth=2,color="#7c7f93")
 
 plt.legend()
 plt.xticks(years)
