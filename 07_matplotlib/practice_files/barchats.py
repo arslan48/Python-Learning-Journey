a5036cffa5a6f13fe92ac27c1062e31674c7388a
Muhammad Arslan <ma7448487@gmail.com>
2026-07-20 06:16:55 +0500

    Update barchats.py

--- 07_matplotlib/practice_files/barchats.py
+++ 07_matplotlib/practice_files/barchats.py
@@ -4,10 +4,11 @@ foods = np.array(["Pizza", "Burger", "Fries", "Pasta", "Sandwich"])
 rating =np.array([9, 8, 7, 8.5, 6])
 plt.tick_params(axis="both",colors="#8839ef")
 
-plt.bar(foods,rating,color="#8839ef")
+plt.bar(foods,rating,color="#8839ef",alpha=0.7)
 plt.title("Food ratings",fontsize=25,family="Georgia",color="#8839ef")
 plt.xlabel("foods",fontsize=16,family="arial",color="#8839ef")
 plt.ylabel("Rating",fontsize=16,family="arial",color="#8839ef")
-plt.grid(axis="y",alpha=0.5)
+plt.grid(axis="y",alpha=0.4)
 
 plt.show()
+
