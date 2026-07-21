ae584e25895e94a348a424a2e7b0f18ee0c1f331
Muhammad Arslan <ma7448487@gmail.com>
2026-07-21 05:40:40 +0500

    Create piechart.py

---
+++ 07_matplotlib/practice_files/piechart.py
@@ -0,0 +1,17 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+activities = ["Sleep", "Study", "Phone/Social Media", "Coding", "Other"]
+hours = np.array([7, 4, 3, 5, 5])
+
+colors=["#dc8a78","#dd7878","#ea76cb","#8839ef","#d20f39"]
+
+plt.title("My Daily Time Distribution",fontsize=20,family="arial",fontweight="bold")
+
+plt.pie(hours,labels=activities,
+	autopct="%1.1f",
+	colors=colors,
+	shadow=False,
+	explode=[0.1,0,0,0,0])
+
+plt.show()
