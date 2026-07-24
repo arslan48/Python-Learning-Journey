846ee3406c5a3087eddae33d521e6d864ba84207
Muhammad Arslan <ma7448487@gmail.com>
2026-07-24 06:54:45 +0500

    Update subplots.py

--- 07_matplotlib/practice_files/subplots.py
+++ 07_matplotlib/practice_files/subplots.py
@@ -4,6 +4,8 @@ import numpy as np
 x=np.array([3,4,2,5,3])
 y=np.array([4,6,3,1,6])
 
+colors=["#dc8a78","#dd7878","#ea76cb","#8839ef","#d20f39"]
+
 figure,axes = plt.subplots(2,2)
 
 axes[0,0].bar(x,y,color="lightgreen")
@@ -15,7 +17,13 @@ axes[0,1].set_title("Plot")
 axes[1,0].scatter(x,y,color="purple")
 axes[1,0].set_title("Scatter Plot")
 
-axes[1,1].plot(x,y,color="skyblue")
-axes[1,1].set_title("Plot")
+axes[1,1].pie(
+	x,
+	labels=y,
+	colors=colors,
+	autopct="%1.1%%",
+	explode=[0,0,0,0,0.1]
+)
+axes[1,1].set_title("Pie")
 
 plt.show()
