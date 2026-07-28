dedf1e64e8acc10babf25ae1089aafb199c6199a
Muhammad Arslan <ma7448487@gmail.com>
2026-07-28 06:31:35 +0500

    Update student_data.py

--- 07_matplotlib/practice_files/student_data.py
+++ 07_matplotlib/practice_files/student_data.py
@@ -5,7 +5,7 @@ df = pd.read_csv("student.csv")
 
 fav_subjects=(df["favorite_subject"].value_counts(ascending=True))
 
-plt.barh(fav_subjects.index,fav_subjects.values,color="lightgreen",ec="black")
+plt.bar(fav_subjects.index,fav_subjects.values,color="lightgreen",ec="black")
 
 plt.title("Favorite Subjects",fontweight="bold")
 
